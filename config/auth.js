81887df7c7044c57d28bff77cf55d11917758b77
waleedbinafzal <waleed_afzal@hotmail.com>
2019-04-24 11:56:12 +0400

    to the github

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '375706783023280',
+		'clientSecret': '0b417021c780aeb30d7d551d83267681',
+		'callbackurl': 'http://localhost:8080/auth/facebook/callback'
+	}
+}
---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url);
+require('./config/passport')(passport);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({secret: 'anystringoftext',
+				saveUninitialized: true, 
+				resave: true}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.set('view engine','ejs');
+
+// app.use('/',function (req,res) {
+// 	// body...
+// 	res.send('Our First Express program!');
+// 	console.log(req.cookies);
+// 	console.log('=================');
+// 	console.log(req.session);
+// });
+
+require('./app/routes.js')(app, passport);
+
+
+app.listen(port);
+console.log('Server running on Port: ' + port);
